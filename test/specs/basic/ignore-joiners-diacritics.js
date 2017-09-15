ccf86481f1c6a9ab35c7128ec555a4fd70e0f19a
Julian Motz <me@julianmotz.com>
2017-09-15 20:38:09 +0200

    Revert everything as v9.0.0 was created based on a stale branch (not up2date)
    Then merge master and re-do all the changes. Especially: new ESLint config

--- test/specs/basic/ignore-joiners-diacritics.js
+++ test/specs/basic/ignore-joiners-diacritics.js
@@ -4,22 +4,22 @@
  * Copyright (c) 2014–2017, Julian Motz
  * Released under the MIT license https://git.io/vwTVl
  *****************************************************/
-"use strict";
-describe("basic mark with ignoreJoiners and diacritics", function () {
-    var $ctx;
-    beforeEach(function (done) {
-        loadFixtures("basic/ignore-joiners-diacritics.html");
+'use strict';
+describe('basic mark with ignoreJoiners and diacritics', function() {
+  var $ctx;
+  beforeEach(function(done) {
+    loadFixtures('basic/ignore-joiners-diacritics.html');
 
-        $ctx = $(".basic-ignore-joiners-diacritics");
-        new Mark($ctx.get()).mark(["Dolor", "Lorem ipsum"], {
-            "separateWordSearch": false,
-            "ignoreJoiners": true,
-            "diacritics": true,
-            "done": done
-        });
+    $ctx = $('.basic-ignore-joiners-diacritics');
+    new Mark($ctx.get()).mark(['Dolor', 'Lorem ipsum'], {
+      'separateWordSearch': false,
+      'ignoreJoiners': true,
+      'diacritics': true,
+      'done': done
     });
+  });
 
-    it("should find matches containing diacritics", function () {
-        expect($ctx.find("mark")).toHaveLength(15);
-    });
+  it('should find matches containing diacritics', function() {
+    expect($ctx.find('mark')).toHaveLength(15);
+  });
 });
